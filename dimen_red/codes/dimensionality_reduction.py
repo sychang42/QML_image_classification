fa4217e9a08dac78d6e0ae8b71373a369c6c4d21
sychang42 <suyeon.chang97@gmail.com>
2023-09-24 11:43:13 +0200

    Add comments

--- dimen_red/codes/dimensionality_reduction.py
+++ dimen_red/codes/dimensionality_reduction.py
@@ -1,3 +1,8 @@
+"""
+Function to perform dimensionality reduction method. 
+Currently, only PCA and convolutional autoencoder are supported.
+"""
+
 import os  
 import sys
 sys.path.append(os.path.dirname(__file__)) 
@@ -5,18 +10,39 @@ sys.path.append(os.path.dirname(__file__))
 import pandas as pd
 import numpy as np 
 
-
 import torch
 from datasets import load_data 
-
 
 from sklearn.decomposition import PCA
 from ae_vanilla import vanilla_autoencoder
 
 import pickle
 
-def dimensionality_reduction(root, data, method, hp, gpu = 0, snapshot_dir = None):
+def dimensionality_reduction(root: str, 
+                             data: str, 
+                             method: str, 
+                             hp: Dict[str, Any], 
+                             gpu: int = 0, 
+                             snapshot_dir: str = None
+                            ) -> Tuple[float] :
+
+    """
+    Perform dimensionality reduction on image data.
+
+    Args:
+        root (str): Root directory containing the image dataset.
+        data (str): Input dataset to apply dimensionality reduction to.
+        method (str): Method for dimensionality reduction. Currently, only PCA and convolutional autoencoder are supported.
+        hp (Dict[str, Any]): Hyperparameters specific to the chosen dimensionality reduction method.
+        gpu (int): ID of the GPU to use for autoencoder training. Set to None to run on CPU.
+        snapshot_dir (str): Directory to store the output data. If None, results will not be saved.
+
+    Returns:
+        train_mse (float): Mean Squared Error (MSE) loss between the original and reconstructed images for the training set.
+        test_mse (float): Mean Squared Error (MSE) loss between the original and reconstructed images for the test set.
+    """
     
+    # Load data. 
     trainds, testds = load_data(root, data)
     
     img_shape = list(trainds.data.shape[1:]) 
@@ -25,6 +51,8 @@ def dimensionality_reduction(root, data, method, hp, gpu = 0, snapshot_dir = Non
         
         X_train = trainds.data.reshape((len(trainds), -1))
         X_train = X_train.numpy() if type(X_train) == torch.Tensor else X_train
+        
+        # If the images are scaled between 0 and 255, rescale them between 0 and 1. 
         if np.max(X_train) > 1. : 
             X_train = X_train/255.
         Y_train = trainds.targets.numpy() if type(trainds.targets) == torch.Tensor else trainds.targets
@@ -45,13 +73,16 @@ def dimensionality_reduction(root, data, method, hp, gpu = 0, snapshot_dir = Non
             
         X_train_pca = pca.transform(X_train)
         X_test_pca = pca.transform(X_test)
-
+        
+        
+        # Rescale the pca output between 0 and 1.
         X_train_pca_rescaled = (X_train_pca - X_train_pca.min())/(X_train_pca.max() - X_train_pca.min())
         X_test_pca_rescaled = (X_test_pca - X_test_pca.min())/(X_test_pca.max() - X_test_pca.min())
 
         
         recons_train =  pca.inverse_transform(X_train_pca)
         recons_test = pca.inverse_transform(X_test_pca)
+        
         train_mse = np.mean((recons_train - X_train)**2) 
         test_mse = np.mean((recons_test - X_test)**2) 
         
@@ -74,10 +105,12 @@ def dimensionality_reduction(root, data, method, hp, gpu = 0, snapshot_dir = Non
         num_epoch = hp['training_params']['num_epoch']
         batch_size = hp['training_params']['batch_size']
         
+        # Load the data into PyTorch dataloader.
         trainloader = torch.utils.data.DataLoader(trainds, batch_size=batch_size, shuffle=True)
         testloader = torch.utils.data.DataLoader(testds, batch_size=batch_size, shuffle=True)
         
         
+        # Device to run the training. 
         device = torch.device("cuda:" + str(gpu) if torch.cuda.is_available() else "cpu")
         model = vanilla_autoencoder(device, hp, snapshot_dir)
         model.train_model(num_epoch, trainloader, testloader)
@@ -89,7 +122,11 @@ def dimensionality_reduction(root, data, method, hp, gpu = 0, snapshot_dir = Non
         train_mse = model.train_loss[np.argmin(model.train_loss)]
         test_mse = model.valid_loss[np.argmin(model.valid_loss)]
                                       
-    
+    else : 
+        raise NotImplementedError("Method Not implemented")
+        
+     
+    # Restore the results in snapshot directory
     if snapshot_dir is not None : 
         df = pd.DataFrame(pred_train[0])
         df['label'] = pred_train[2]
