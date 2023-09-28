7f8e5a682ee8308415f06806b86c0d7ed7138870
sychang42 <suyeon.chang97@gmail.com>
2023-09-28 18:25:37 +0200

    Update sphinx doc

--- src/quantum_classifier/models/train.py
+++ src/quantum_classifier/models/train.py
@@ -117,31 +117,29 @@ def train(
         train_args (Dict[str, Any]): Hyperparameters needed for classifier training.
             The dictionary should contain the following points:
 
-            - *num_epochs*: Integer indicating the number of training epochs.
-            - *batch_size*: Integer indicating the training batch size.
-            - *loss_type*: List of strings representing the loss types used in the
+            * *num_epochs*: Integer indicating the number of training epochs.
+            * *batch_size*: Integer indicating the training batch size.
+            * *loss_type*: List of strings representing the loss types used in the
             training. Currently, only :func:`metrics.BCE_loss` and
-            :func:`metrics.accuracy` are supported.
-
-            - *seed*: Seed used to generate random values using JAX's random number
-            generator (:class:`jax.random`).
+                :func:`metrics.accuracy` are supported.
+            * *seed*: Seed used to generate random values using JAX's random number
+                generator (:class:`jax.random`).
 
         model_args (Dict[str, Any]): Arguments required to constructed the QCNN.
 
-            - *num_wires*: Number of qubits in the QCNN.
-            - *num_measured*:Number of measured qubits at the end of the circuit.
-            For L classes, we measure :math:`\lceil (\log_2(L))\rceil` qubits.
-
-            - *trans_inv*: Boolean to indicate whether the QCNN is
-            translational invariant or not. If True, all filters in a layer share
-            identical parameters; otherwise, different parameters are used. (To be
-            implemented)
+            * *num_wires*: Number of qubits in the QCNN.
+            * *num_measured*:Number of measured qubits at the end of the circuit.For
+                L classes, we measure :math:`\lceil (\log_2(L))\rceil` qubits.
+            * *trans_inv*: Boolean to indicate whether the QCNN is
+                translational invariant or not. If True, all filters in a layer share
+                identical parameters; otherwise, different parameters are used. (To be
+                implemented)
 
         optim_args (Dict[str, float]): :class:`optax.adam` optimizer hyperparameters.
 
-            - *learning_rate*: Learning rate for the optimizer.
-            - *b1*: :math:`\beta_1` value of the Adam optimizer.
-            - *b2*: :math:`\beta_2` value of the Adam optimizer.
+            * *learning_rate*: Learning rate for the optimizer.
+            * *b1*: :math:`\beta_1` value of the Adam optimizer.
+            * *b2*: :math:`\beta_2` value of the Adam optimizer.
 
         snapshot_dir (str, optional): Directory to store the training result if not
             None. Defaults to None.
