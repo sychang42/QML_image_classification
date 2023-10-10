1bf5924a2d8a820d19710851c5e112187bfbf20a
sychang42 <suyeon.chang97@gmail.com>
2023-10-10 17:50:47 +0200

    Update documentation

--- src/dimen_red/load_data.py
+++ src/dimen_red/load_data.py
@@ -1,37 +1,38 @@
-"""
+r"""
 Function to load the given data and return PyTorch dataset.
 """
+
+import os
+import sys
+
+sys.path.append(os.path.dirname(__file__))
+
 import torch
 import torchvision
 from datasets import SAT4, EuroSAT
 from torchvision.transforms import transforms
 from typing import Optional, Callable, Tuple
 
-import os
-import sys
-sys.path.append(os.path.dirname(__file__))
 
-
-def load_data(root: str,
-              data: str,
-              download: Optional[bool] = False,
-              transform: Optional[Callable] = None
-              ) -> Tuple[torch.utils.data.Dataset]:
-    """
-    Load the specified dataset and create PyTorch datasets.
+def load_data(
+    root: str,
+    data: str,
+    download: Optional[bool] = False,
+    transform: Optional[Callable] = None,
+) -> Tuple[torch.utils.data.Dataset]:
+    r""" Load the specified dataset and create PyTorch datasets.
 
     Args:
-        root (str): The root directory where the dataset is located. 
-                    If the dataset does not exist locally, it will be downloaded here.
+        root (str): The root directory where the dataset is located.
+            If the dataset does not exist locally, it will be downloaded here.
         data (str): The name of the dataset to load.
-        download (bool): Boolean to indicate whether to download the dataset 
-                         if it's not already available locally.
-        transform (Optional[Callable]): A PyTorch transform function that takes a PIL image and returns the transformed version. 
-                                        If None, it defaults to transform.ToTensor().
+        download (bool): Boolean to indicate whether to download the dataset
+            if it's not already available locally.
+        transform (Optional[Callable]): A PyTorch transform function that takes a PIL image and 
+            returns the transformed version.If None, it defaults to transform.ToTensor().
 
     Returns:
-        trainds (torch.utils.data.Dataset): The training dataset.
-        testds (torch.utils.data.Dataset]): The testing dataset.
+        Tuple[torch.utils.data.Dataset]: Tuple of the training and the the testing dataset.
     """
 
     if transform is None:
@@ -41,17 +42,15 @@ def load_data(root: str,
         "MNIST": torchvision.datasets.MNIST,
         "FashionMNIST": torchvision.datasets.FashionMNIST,
         "SAT4": SAT4,
-        "EuroSAT": EuroSAT
+        "EuroSAT": EuroSAT,
     }
 
     dataset = switcher.get(data, lambda: None)
 
     if dataset is not None:
-        trainds = dataset(root=root, train=True,
-                          download=download, transform=transform)
-        testds = dataset(root=root, train=False,
-                         download=download, transform=transform)
+        trainds = dataset(root=root, train=True, download=download, transform=transform)
+        testds = dataset(root=root, train=False, download=download, transform=transform)
     else:
         raise NotImplementedError("Dataset not supported.")
 
-    return trainds, testds
+    return trainds, testds  # type: ignore
